b54f2ae5de8c873fb18e065947bf7d168f619a9e
Raheel Jamshaid <76648311+raheelProjects@users.noreply.github.com>
2024-10-22 12:03:25 +0200

    learning ngFor and ngIf
    making table showing data if intended or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
+interface Guest {
+  id: number;
+  name: string;
+  isInvited: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'les4_ngIfAndngFor';
+  title = 'lesson 4 : ngIf And ngFor';
+  showInvite: boolean = true;
+  guests: Guest[] = [
+    {
+      id: 0,
+      name: 'Bisham',
+      isInvited: true,
+    },
+    {
+      id: 1,
+      name: 'Hassan',
+      isInvited: true,
+    },
+    {
+      id: 2,
+      name: 'Zaid',
+      isInvited: true,
+    },
+    {
+      id: 3,
+      name: 'Freddy',
+      isInvited: false,
+    },
+    {
+      id: 4,
+      name: 'Oatmar',
+      isInvited: false,
+    },
+  ];
+
+  changeInvite(id: number, invited: boolean) {
+    this.guests = this.guests.map((guest) =>
+      guest.id === id ? { ...guest, isInvited: !invited } : guest
+    );
+  }
+
+  changeShowStatus() {
+    this.showInvite = !this.showInvite;
+  }
 }
